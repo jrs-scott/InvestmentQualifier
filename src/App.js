33fd8c1cab99002d6b548560c63bfa55404cd206
Jamie Scott <north.jrn@gmail.com>
2021-02-06 12:20:58 -0800

    Add bootstrap and configure initial routing with temp navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import './App.css';
-import Assessment from './Assessment.js';
+import {
+  Route,
+  NavLink,
+  BrowserRouter,
+  Switch
+} from 'react-router-dom';
+import Assessment from './Assessment';
+import AccountCreation from './AccountCreation';
+import Rejection from './Rejection';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   // TODO: Add navbar for design purposes only and set up routing
-  
+
   return (
-    <div id="main-content">
-      <Assessment /> {/* Landing page of the application */}
-    </div>
+    <BrowserRouter basename="/">
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand><img class="header-logo" src="/logo.jpg" alt="Company logo" /> ACME Investing</Navbar.Brand>
+          {/* TODO: Remove nav links after development */}
+          <Nav className="ml-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/accountcreation">Account Creation</Nav.Link>
+            <Nav.Link href="/rejection">Rejection</Nav.Link>
+          </Nav>
+        </Navbar>
+        
+        <div id="main-content">
+          <Switch>
+            <Route path='/accountcreation'>
+              <AccountCreation />
+            </Route>
+            <Route path='/rejection'>
+              <Rejection />
+            </Route>
+            <Route exact path='/'>
+              <Assessment />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
