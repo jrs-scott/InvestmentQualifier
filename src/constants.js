48370efa72a26b962be83a43b4bf6ee9c49cff92
Jamie Scott <north.jrn@gmail.com>
2021-02-16 23:35:40 -0800

    Add tests for data service

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,12 @@
-export const approvalMsg = "Congrats, you have been pre-approved!"
-export const rejectionMsg = "Nullam odio lectus, molestie at laoreet ac, fermentum in augue."
+const approvalMsg = "Congrats, you have been pre-approved!"
+
+const rejectionMsg = "Nullam odio lectus, molestie at laoreet ac, fermentum in augue."
   + " Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent eu ultrices urna."
-export const errorMsg = "An error has occurred...";
+  
+const errorMsg = "An error has occurred...";
+
+export default {
+  approvalMsg, 
+  rejectionMsg, 
+  errorMsg
+};
---
+++ src/data-service.test.js
@@ -0,0 +1,68 @@
+import { assessQualifications } from './data-service';
+import { approvalMsg, errorMsg, rejectionMsg } from "./constants";
+
+let mockData;
+
+// Reset mock data for each test
+beforeEach(() => {
+  mockData = {
+    investmentAmount: '1000.00',
+    investmentType: 'Stocks',
+    netWorth: '100000.00',
+    annualIncome: '100000.00',
+    creditScore: '800'
+  };
+});
+
+it('should approve valid requests', async () => {
+  const resp = await assessQualifications(mockData);
+  console.log(resp);
+
+  expect(resp.qualified).toBe(true);
+  expect(resp.message).toBe(approvalMsg);
+  expect(resp.error).toBe(false);
+});
+
+it('should return an error for excessive investment amounts', async () => {
+  mockData.investmentAmount = '9000001';
+
+  const resp = await assessQualifications(mockData);
+  console.log(resp);
+
+  expect(resp.qualified).toBe(false);
+  expect(resp.message).toBe(errorMsg);
+  expect(resp.error).toBe(true);
+});
+
+it('should reject credit scores below 600', async () => {
+  mockData.creditScore = '599';
+
+  const resp = await assessQualifications(mockData);
+  console.log(resp);
+
+  expect(resp.qualified).toBe(false);
+  expect(resp.message).toBe(rejectionMsg);
+  expect(resp.error).toBe(false);
+});
+
+it('should reject investment requests over 1/5 of annual income', async () => {
+  mockData.investmentAmount = '25000';
+
+  const resp = await assessQualifications(mockData);
+  console.log(resp);
+
+  expect(resp.qualified).toBe(false);
+  expect(resp.message).toBe(rejectionMsg);
+  expect(resp.error).toBe(false);
+});
+
+it('should reject investment requests over 3% of total networth', async () => {
+  mockData.investmentAmount = '3500';
+
+  const resp = await assessQualifications(mockData);
+  console.log(resp);
+
+  expect(resp.qualified).toBe(false);
+  expect(resp.message).toBe(rejectionMsg);
+  expect(resp.error).toBe(false);
+});
